5ac91b7d206cf6707f42680de8d116f661340ec2
azizularif420 <arif@mazegeek.com>
2025-10-03 18:18:00 +0600

    event and route

---
+++ app/Events/ChatMessageEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\ChatMessage;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatMessageEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $chatMessage;
+
+    public function __construct(ChatMessage $chatMessage)
+    {
+        $this->chatMessage = $chatMessage->load('user');
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('ticket.' . $this->chatMessage->ticket_id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'chat.message';
+    }
+}
